5442678b6aa3f3602ce3365f1de5987f95b45235
WatchRinseRepeat <s.a.eyring@gmail.com>
2023-04-08 09:27:04 -0600

    created Project_1_alert files

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+var A = 'This is a string, it says \"Hello\"'; //Creating a variable with a string
+var B = 'This is ' + 'Concatenated.'; //Creating a variable a assigning it a concatenated string
+
+window.alert(A + ' and went on a fraying rant.'); //popping up an alert and concatenating a string and a variable
+document.write(B); //Writing onto the document the value of a variable
+
+var Family = "The Addams", Dad = "Gomez", Mom = "Morticia", Daughter = "Wednesday",
+    Son = 'Pugsley';  //creating multiple variables.
+
+document.write(Dad); //Writing a variable's value to the document.
+
+2 + 2; //an expression
