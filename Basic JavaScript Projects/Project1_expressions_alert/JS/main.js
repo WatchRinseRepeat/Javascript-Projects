9d3a3ea7d2e5e3aca5ce618c6ed8b06cc9897bd3
WatchRinseRepeat <s.a.eyring@gmail.com>
2023-04-08 09:30:52 -0600

    make variable names conform to assignment

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,8 +1,8 @@
-var A = 'This is a string, it says \"Hello\"'; //Creating a variable with a string
-var B = 'This is ' + 'Concatenated.'; //Creating a variable a assigning it a concatenated string
+var Sent1 = 'This is a string,'; //Creating a variable with a string
+var Sent2 = ',it says ' + ' it says \"Hello\" to '; //Creating a variable a assigning it a concatenated string
 
-window.alert(A + ' and went on a fraying rant.'); //popping up an alert and concatenating a string and a variable
-document.write(B); //Writing onto the document the value of a variable
+window.alert(Sent1 + Sent2); //popping up an alert and concatenating a string and a variable
+document.write(Sent1 + Sent2); //Writing onto the document the value of a variable
 
 var Family = "The Addams", Dad = "Gomez", Mom = "Morticia", Daughter = "Wednesday",
     Son = 'Pugsley';  //creating multiple variables.
